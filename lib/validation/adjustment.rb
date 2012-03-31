9c3409ac2d2dcf78105295c6592ac5f53231fed7
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2012-04-01 05:23:17 +0900

    fix namespace Adjustment::Builders

--- lib/validation/adjustment.rb
+++ lib/validation/adjustment.rb
@@ -22,56 +22,56 @@ module Validation
           end
         end
       end
-    end
-
-    # Apply adjuster when passed condition.
-    # @return [lambda]
-    def WHEN(condition, adjuster)
-      raise TypeError, 'wrong object for condition' unless conditionable? condition
-      raise TypeError, 'wrong object for adjuster' unless adjustable? adjuster
       
-      ->v{pass?(v, condition) ? adjuster.call(v) : v}
-    end
-    
-    # Sequencial apply all adjusters.
-    # @return [lambda]
-    def INJECT(adjuster1, adjuster2, *adjusters)
-      adjusters = [adjuster1, adjuster2, *adjusters]
-
-      unless adjusters.all?{|f|adjustable? f}
-        raise TypeError, 'wrong object for adjuster'
-      end
-
-      ->v{
-        adjusters.reduce(v){|ret, adjuster|adjuster.call ret}
-      }
-    end
-
-    # Accept any parser when that resopond to parse method.
-    # @return [lambda]
-    def PARSE(parser)
-      if !::Integer.equal?(parser) and !parser.respond_to?(:parse)
-        raise TypeError, 'wrong object for parser'
+      # Apply adjuster when passed condition.
+      # @return [lambda]
+      def WHEN(condition, adjuster)
+        raise TypeError, 'wrong object for condition' unless conditionable? condition
+        raise TypeError, 'wrong object for adjuster' unless adjustable? adjuster
+        
+        ->v{pass?(v, condition) ? adjuster.call(v) : v}
       end
       
-      ->v{
-        if ::Integer.equal? parser
-          ::Kernel.Integer v
-        else
-          parser.parse(
-            case v
-            when String
-              v
-            when ->_{v.respond_to? :to_str}
-              v.to_str
-            when ->_{v.respond_to? :read}
-              v.read            
-            else
-              raise TypeError, 'wrong object for parsing source'
-            end
-          )
+      # Sequencial apply all adjusters.
+      # @return [lambda]
+      def INJECT(adjuster1, adjuster2, *adjusters)
+        adjusters = [adjuster1, adjuster2, *adjusters]
+
+        unless adjusters.all?{|f|adjustable? f}
+          raise TypeError, 'wrong object for adjuster'
         end
-      }
+
+        ->v{
+          adjusters.reduce(v){|ret, adjuster|adjuster.call ret}
+        }
+      end
+
+      # Accept any parser when that resopond to parse method.
+      # @return [lambda]
+      def PARSE(parser)
+        if !::Integer.equal?(parser) and !parser.respond_to?(:parse)
+          raise TypeError, 'wrong object for parser'
+        end
+        
+        ->v{
+          if ::Integer.equal? parser
+            ::Kernel.Integer v
+          else
+            parser.parse(
+              case v
+              when String
+                v
+              when ->_{v.respond_to? :to_str}
+                v.to_str
+              when ->_{v.respond_to? :read}
+                v.read            
+              else
+                raise TypeError, 'wrong object for parsing source'
+              end
+            )
+          end
+        }
+      end
     end
 
     # Useful Adjusters

26afe583cd832d3344560a6e2b5a4fe63009ac41
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2012-08-25 08:27:31 +0900

    improve

---
+++ lib/validation/requirements.rb
@@ -0,0 +1,7 @@
+require_relative 'version'
+require_relative 'errors'
+require_relative 'condition'
+require_relative 'adjustment'
+require_relative 'validatable'
+require_relative 'singleton_class'
+require_relative 'module_functions'
